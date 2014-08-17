d39baca4bed5b874386ff834521eb3ab15cb6a23
thiendev1337 <thiendev1337@gmail.com>
2014-08-17 19:58:13 +0700

    date server

--- date-server/src/main/java/DateServer.java
+++ date-server/src/main/java/DateServer.java
@@ -11,13 +11,14 @@ import java.util.Date;
  */
 public class DateServer {
     public static void main(String[] args) {
-        try (
-                ServerSocket server = new ServerSocket(9090);
-                Socket socket = server.accept();
-        ) {
-            Writer out = new OutputStreamWriter(socket.getOutputStream(), "ASCII");
-            out.write(new Date() + "\r\n");
-            out.flush();
+        try (ServerSocket server = new ServerSocket(9090)) {
+            while (true) {
+                try (Socket socket = server.accept()) {
+                    Writer out = new OutputStreamWriter(socket.getOutputStream());
+                    out.write(new Date() + "\r\n");
+                    out.flush();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
